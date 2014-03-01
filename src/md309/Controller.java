47f9d4ba5cc29e82bb2e79c98d92b868b45cf12a
Scott Moore <scott@thinkmoore.net>
2014-03-01 16:12:47 -0500

    Capture images an select a polygon

---
+++ src/md309/Controller.java
@@ -0,0 +1,108 @@
+package md309;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
+import org.opencv.core.Point;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Controller implements Initializable {
+    @FXML
+    private ImageView camview;
+    @FXML
+    private Pane imagepane;
+    @FXML
+    private Polygon bounds;
+
+    private ImageGrabber ig;
+
+    public void initialize(URL location, ResourceBundle resources) {
+        ig = new ImageGrabber();
+        for (int i = 0; i < bounds.getPoints().size(); i += 2) {
+            Circle handle = new Circle(bounds.getPoints().get(i), bounds.getPoints().get(i+1), 5);
+            handle.setFill(Color.WHITE);
+            handle.setStroke(Color.BLACK);
+            handle.setStrokeWidth(1);
+            setDragHandler(handle);
+            final AtomicInteger polyCoordinateIndex = new AtomicInteger(i);
+            handle.centerXProperty().addListener(new ChangeListener<Number>() {
+                @Override
+                public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                    bounds.getPoints().set(polyCoordinateIndex.get(), newValue.doubleValue());
+                }
+            });
+            handle.centerYProperty().addListener(new ChangeListener<Number>() {
+                @Override
+                public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                    bounds.getPoints().set(polyCoordinateIndex.get() + 1, (Double) newValue);
+                }
+            });
+            imagepane.getChildren().add(handle);
+        }
+    }
+
+    private double dragDeltaX, dragDeltaY;
+
+    private void setDragHandler(final Circle circle) {
+        circle.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent mouseEvent) {
+                dragDeltaX = circle.getCenterX() - mouseEvent.getSceneX();
+                dragDeltaY = circle.getCenterY() - mouseEvent.getSceneY();
+            }
+        });
+
+        circle.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent mouseEvent) {
+                circle.setCenterX( mouseEvent.getSceneX() + dragDeltaX );
+                circle.setCenterY( mouseEvent.getSceneY() + dragDeltaY );
+                circle.setCursor(Cursor.MOVE);
+            }
+        });
+
+        circle.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent mouseEvent) {
+                circle.setCursor(Cursor.HAND);
+            }
+        });
+
+        circle.setOnMouseReleased(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent mouseEvent) {
+                circle.setCursor(Cursor.HAND);
+            }
+        });
+    }
+
+    public void doCapture(ActionEvent event) {
+        try {
+            Image i = ig.grab();
+            camview.setImage(ig.grab());
+        } catch (IOException ioe) {
+            System.err.println("Failed to grab image: " + ioe);
+            ioe.printStackTrace();
+        }
+    }
+
+    public void doTransform(ActionEvent event) {
+        Double[] points = new Double[8];
+        ig.setTransform(bounds.getPoints().toArray(points));
+    }
+
+    public void doReset(ActionEvent event) {
+        ig.resetTransform();
+    }
+}
---
+++ src/md309/ImageGrabber.java
@@ -0,0 +1,53 @@
+package md309;
+
+import javafx.scene.image.Image;
+import javafx.scene.shape.Polygon;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.Point;
+import org.opencv.highgui.Highgui;
+import org.opencv.highgui.VideoCapture;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+/**
+ * Created by scott on 3/1/14.
+ */
+public class ImageGrabber {
+    private VideoCapture vc;
+    private Point[] bounds;
+
+    public ImageGrabber() {
+        // Should take device as a parameter
+        vc = new VideoCapture(0);
+
+    }
+
+    public void setTransform(Double[] points) {
+        bounds = new Point[4];
+        for (int i = 0; i < 4; i++) {
+            Point p = new Point();
+            p.x = points[2 * i];
+            p.y = points[2 * i + 1];
+            bounds[i] = p;
+        }
+    }
+
+    public void resetTransform() {
+        bounds = null;
+    }
+
+    public Image grab() throws IOException {
+        Mat m = new Mat();
+        vc.read(m);
+
+        if (bounds != null) {
+            // DO THE TRANSFORM
+        }
+
+        MatOfByte mb = new MatOfByte();
+        Highgui.imencode(".jpg",m,mb);
+        return new Image(new ByteArrayInputStream(mb.toArray()));
+    }
+}
