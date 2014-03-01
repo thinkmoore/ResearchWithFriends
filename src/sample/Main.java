732814e3aee5985fa0761cd81826a45dd3cd4e8e
Aslan Askarov <aslan@askarov.net>
2014-03-01 17:42:06 -0500

    Putting in logging infrastructure

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,6 +18,11 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+        TestDebugger td = new TestDebugger();
+        td.test();
+
         launch(args);
+
+
     }
 }
